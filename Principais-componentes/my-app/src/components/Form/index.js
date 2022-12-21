a1ac2166c5250bd3d9d2ce0c673edf8d87f25866
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-12-21 11:00:56 -0300

    utilizando api do react native para gerar vibrações no dispositivo

--- Principais-componentes/my-app/src/components/Form/index.js
+++ Principais-componentes/my-app/src/components/Form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Vibration } from "react-native";
 import ResultImc from "./ResultImc/"
 import styles from "./style";
 
@@ -10,10 +10,16 @@ export default function Form(){
     const [messageImc, setMessageImc] = useState("Preencha o peso e altura")
     const [imc, setImc] = useState(null)
     const [textButton, setTextButton] = useState("Calcular")
+    const [errorMessage, setErrorMessage] = useState(null)
 
 
     function imcCalculator() {
         return setImc((weight/(height*height)).toFixed(2))
+    }
+
+    function verificationImc() {
+        Vibration.vibrate()
+        setErrorMessage("Campo obrigatório*")
     }
 
     function validationImc() {
@@ -24,10 +30,11 @@ export default function Form(){
             setWeight(null)
             setMessageImc("Seu imc é igual:")
             setTextButton("Calcular novamente")
+            setErrorMessage(null)
             return
         }
-
         setImc(null)
+        verificationImc()
         setTextButton("Calcular")
         setMessageImc("Preencha o peso e altura")
     }
@@ -36,6 +43,7 @@ export default function Form(){
         <View style={styles.formContext}>
             <View style={styles.form}>
                 <Text style={styles.formLabel}>Altura</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput
                 style={styles.input}
                 onChangeText={setHeight}
@@ -45,6 +53,7 @@ export default function Form(){
                 ></TextInput>
 
                 <Text style={styles.formLabel}>Peso</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
                 <TextInput
                 style={styles.input}
                 value={weight}
