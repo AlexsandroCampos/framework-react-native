a158b3698b5a364a7a9c4b803a9aed4c9c0f93fa
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-12-21 11:33:19 -0300

    utilizando a api share

--- Principais-componentes/my-app/src/components/Form/ResultImc/style.js
+++ Principais-componentes/my-app/src/components/Form/ResultImc/style.js
@@ -4,7 +4,6 @@ const styles = StyleSheet.create({
     resultImc: {
         flex: 1,
         marginTop: 15,
-        paddingTop: 60,
         borderRadius: 50,
         alignItems: "center",
         width: "100%",
@@ -19,8 +18,23 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "#FF0043",
         fontWeight: 'bold'
+    },
+    boxShareButton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10
+    },
+    shared: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 5,
+    },
+    sharedText: {
+        color: "#fff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
     }
-    
 });
 
 export default styles
